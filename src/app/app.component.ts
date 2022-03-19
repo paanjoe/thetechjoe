dd9dc4004f74d145013c73bfa40b14cc26a58978
PaanJoe <farhan.gunz@gmail.com>
2022-03-19 16:42:33 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import * as Helper from './helper/enum';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public helper: any = Helper.helper;
+}
---
+++ src/app/helper/enum.ts
@@ -0,0 +1,7 @@
+export enum helper {
+    HEADER_TITLE = "TheTechJoe",
+    HEADER_SUBTITLE_TOP = "Hello! I am",
+    HEADER_SLOGAN = "Full Stack Software Developer | A Ninja Father and Husband",
+    HEADER_DESCRIPTION = "I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at",
+    HEADER_BUTTON = "About Me"
+}
