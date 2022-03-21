7f636fda6a9d37fe2a440960ae71f3aa0e952773
PaanJoe <farhan.gunz@gmail.com>
2022-03-22 00:11:54 +0800

    added backend and enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import * as Helper from './helper/enum';
+import data from './helper/stack.json';
+import experiences from './helper/experiences.json'
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,19 @@ import * as Helper from './helper/enum';
 export class AppComponent {
   public helper: any = Helper.helper;
   public currentYear: number = new Date().getFullYear();
+  public stackList = (<any>data).data;
+  public expList = (experiences).experiences;
+  public expPosition: string = '';
+  public expJobScope: any = [];
+
+
+  ngOnInit() {
+    this.viewexp(this.expList[0].position, this.expList[0].jobscope)
+  }
+
+  viewexp(position: string, jobscope: any) {
+    this.expPosition = position;
+    this.expJobScope = jobscope;
+    console.log(this.expJobScope[0].item)
+  }
 }
--- src/app/helper/enum.ts
+++ src/app/helper/enum.ts
@@ -1,7 +1,33 @@
 export enum helper {
-    HEADER_TITLE = "TheTechJoe",
+    // HEADER
+    HEADER_TITLE = "<TheTechJoe/>",
     HEADER_SUBTITLE_TOP = "Hello! I am",
     HEADER_SLOGAN = "Full Stack Software Developer | A Ninja Father and Husband",
-    HEADER_DESCRIPTION = "I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at",
-    HEADER_BUTTON = "About Me"
+    HEADER_DESCRIPTION = "🇲🇾-based Software Engineer who love to speak in Typescript. I love to build web apps that I think it might be useful to everyone! A 👨‍💻 slave who consistently helping to improve Organization Digitalization agenda 🤩. Currently trying to master AI 🤖 and also trying to be an awesome father and husband 👨‍👩‍👧‍👦.",
+    HEADER_BUTTON = "About Me",
+
+    // ABOUT ME
+    ABOUT_TITLE = "<About Me/>",
+    ABOUT_TITLE_PARA = "Bonjour le monde",
+    ABOUT_PARA = "Hey guys! My name is Farhan Fazli and I love to code and learn new technology stuff. I have an interest in Web and Mobile development! I'm also an Anime lover :). I've worked with multiple organization and always put organization digital transformation objective as my motivation to keep learning. I also love to build random apps that can be used by everyone and also to showcase my capabilities and inspiration!",
+    ABOUT_PICTURE = "https://scontent.fkul8-1.fna.fbcdn.net/v/t1.6435-9/69980685_3492342597457954_980635909504368640_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=NuSIPzp_VwYAX_l3Nq5&tn=Se7QiE85Jb-QIg3h&_nc_ht=scontent.fkul8-1.fna&oh=00_AT_LCnkYVbGga4ck2KCIY_KFVYeWSBft3EqAKY9M9WMcGg&oe=625F6AAC",
+    ABOUT_STACK_TITLE = "Stack",
+
+    // EXPERIENCES
+    EXP_TITLE = "<Professional Experiences/>",
+
+    // GITHUB
+    GH_TITLE = "<GitHub Repositories/>",
+
+    // FEATURED
+    FEATURED_TITLE = "<Featured Projects/>",
+
+    // CONTACT
+    CONTACT_TITLE = "<Get In Touch/>",
+    CONTACT_DOWNLOAD_ICON = "URL HERE",
+    CONTACT_LINKEDIN_ICON = "URL HERE",
+    CONTACT_GITHUB_ICON = "URL HERE",
+    CONTACT_MEDIUM_ICON = "URL HERE",
+    CONTACT_DEVTO_ICON = "URL HERE",
+    CONTACT_BUTTON_SEND = "Send Message"
 }
